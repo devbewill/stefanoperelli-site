d099f5af898e5cf2d25a8035164932f8b1aad46c
devbewill <sedprc@gmail.com>
2026-03-05 11:40:39 +0100

    fallback costants

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -164,9 +164,11 @@ declare module 'astro:content' {
 	type DataEntryMap = {
 		"blog": Record<string, {
   id: string;
-  body?: string;
+  render(): Render[".md"];
+  slug: string;
+  body: string;
   collection: "blog";
-  data: any;
+  data: InferEntrySchema<"blog">;
   rendered?: RenderedContent;
   filePath?: string;
 }>;
@@ -210,6 +212,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = never;
+	export type ContentConfig = typeof import("../src/content/config.js");
 	export type LiveContentConfig = never;
 }
