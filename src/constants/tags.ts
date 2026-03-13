a3edc3db3e74c59a3f7f86766b9eb32ec65f1d6f
devbewill <sedprc@gmail.com>
2026-03-13 11:33:20 +0100

    added description

--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -1,11 +1,20 @@
 export const TAG_STYLES = {
   post: {
     label: "POST",
+    backgroundColor: "bg-orange-100",
+    textColor: "text-orange-800",
+    borderColor: "border-orange-300",
   },
   project: {
     label: "PRG",
+    backgroundColor: "bg-blue-100",
+    textColor: "text-blue-800",
+    borderColor: "border-blue-300",
   },
   link: {
     label: "LINK",
+    backgroundColor: "bg-pink-100",
+    textColor: "text-pink-800",
+    borderColor: "border-pink-300",
   },
 } as const;
